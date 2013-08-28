acfc6cb7f3f2ee27d1cc93b98571799e4428c5b8
Timurhan Sungur <timurhan.s@gmail.com>
2013-08-28 11:10:44 +0200

    Stream fix
    Stream is closed automatically so it is contained in a buffer until it
    is sent

--- src/main/java/org/jclouds/orion/blobstore/OrionBlobStore.java
+++ src/main/java/org/jclouds/orion/blobstore/OrionBlobStore.java
@@ -1,7 +1,10 @@
 package org.jclouds.orion.blobstore;
 
+import java.io.IOException;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.jclouds.blobstore.BlobStoreContext;
 import org.jclouds.blobstore.domain.Blob;
 import org.jclouds.blobstore.domain.BlobMetadata;
@@ -33,176 +36,186 @@ import com.google.inject.Inject;
 
 public class OrionBlobStore extends BaseBlobStore {
 
-	private final OrionApi api;
-	private final String workspaceName;
-	private final BlobToOrionBlob blob2OrionBlob;
-	private final BlobPropertiesToBlobMetadata blobProps2BlobMetadata;
-	private final BlobUtils blobUtils;
-	private final Factory orionBlobProvider;
+    private final OrionApi api;
+    private final String workspaceName;
+    private final BlobToOrionBlob blob2OrionBlob;
+    private final BlobPropertiesToBlobMetadata blobProps2BlobMetadata;
+    private final BlobUtils blobUtils;
+    private final Factory orionBlobProvider;
 
-	@Inject
-	protected OrionBlobStore(BlobStoreContext context,
-			@Provider Supplier<Credentials> creds, BlobUtils blobUtils,
-			OrionApi api, Supplier<Location> defaultLocation,
-			@Memoized Supplier<Set<? extends Location>> locations,
-			BlobToOrionBlob blob2OrionBlob,
-			BlobPropertiesToBlobMetadata blobProps2BlobMetadata,
-			OrionBlob.Factory orionBlobProvider) {
-		super(context, blobUtils, defaultLocation, locations);
-		this.blobUtils = blobUtils;
-		this.api = Preconditions.checkNotNull(api, "api is null");
-		this.workspaceName = Preconditions.checkNotNull(creds.get(),
-				"creds is null").identity;
-		this.blob2OrionBlob = Preconditions.checkNotNull(blob2OrionBlob,
-				"blob2OrionBlob is null");
-		this.blobProps2BlobMetadata = Preconditions.checkNotNull(
-				blobProps2BlobMetadata, "blobProps2BlobMetadata is null");
-		this.orionBlobProvider = Preconditions.checkNotNull(orionBlobProvider,
-				"orionBlobProvider is null");
+    @Inject
+    protected OrionBlobStore(BlobStoreContext context,
+	    @Provider Supplier<Credentials> creds, BlobUtils blobUtils,
+	    OrionApi api, Supplier<Location> defaultLocation,
+	    @Memoized Supplier<Set<? extends Location>> locations,
+	    BlobToOrionBlob blob2OrionBlob,
+	    BlobPropertiesToBlobMetadata blobProps2BlobMetadata,
+	    OrionBlob.Factory orionBlobProvider) {
+	super(context, blobUtils, defaultLocation, locations);
+	this.blobUtils = blobUtils;
+	this.api = Preconditions.checkNotNull(api, "api is null");
+	this.workspaceName = Preconditions.checkNotNull(creds.get(),
+		"creds is null").identity;
+	this.blob2OrionBlob = Preconditions.checkNotNull(blob2OrionBlob,
+		"blob2OrionBlob is null");
+	this.blobProps2BlobMetadata = Preconditions.checkNotNull(
+		blobProps2BlobMetadata, "blobProps2BlobMetadata is null");
+	this.orionBlobProvider = Preconditions.checkNotNull(orionBlobProvider,
+		"orionBlobProvider is null");
+    }
+
+    @Override
+    public boolean blobExists(String container, String blobName) {
+	// TODO Auto-generated method stub
+	throw new IllegalStateException("Not yet implemented.");
+    }
+
+    @Override
+    public BlobMetadata blobMetadata(String container, String blobName) {
+	String parentPath = OrionUtils.fetchParentPath(blobName);
+	// Blob names must not start with a "/" since they are relative paths
+	// they will be automatically removed in case it starts with that
+	// Get the blob name
+	// Convert the blob name to it's metadata file name and fetch it
+	return blobProps2BlobMetadata.apply(api.getMetadata(getUserLocation(),
+		container, parentPath, OrionUtils
+			.getMetadataFileName(OrionUtils.fetchName(blobName,
+				parentPath))));
+    }
+
+    @Override
+    public boolean containerExists(String container) {
+	return api.containerExists(getUserLocation(), container);
+    }
+
+    @Override
+    public void deleteContainer(String container) {
+	api.deleteContainer(getUserLocation(), container);
+    }
+
+    @Override
+    public boolean createContainerInLocation(Location location, String container) {
+	return this.api.createContainerAsAProject(getUserLocation(), container);
+    }
+
+    @Override
+    public boolean createContainerInLocation(Location arg0, String arg1,
+	    CreateContainerOptions arg2) {
+	// TODO Auto-generated method stub
+	throw new IllegalStateException("Not yet implemented.");
+    }
+
+    @Override
+    protected boolean deleteAndVerifyContainerGone(String container) {
+	return api.deleteContainer(getUserLocation(), container);
+    }
+
+    @Override
+    public Blob getBlob(String arg0, String arg1, GetOptions arg2) {
+	// TODO Auto-generated method stub
+	throw new IllegalStateException("Not yet implemented.");
+    }
+
+    private String getUserLocation() {
+	return this.workspaceName;
+    }
+
+    @Override
+    public PageSet<? extends StorageMetadata> list() {
+	// TODO Auto-generated method stub
+	throw new IllegalStateException("Not yet implemented.");
+    }
+
+    @Override
+    public PageSet<? extends StorageMetadata> list(String arg0,
+	    ListContainerOptions arg1) {
+	// TODO Auto-generated method stub
+	throw new IllegalStateException("Not yet implemented.");
+    }
+
+    @Override
+    public String putBlob(String container, Blob blob) {
+	OrionBlob orionBlob = this.blob2OrionBlob.apply(blob);
+	// Copy temporarily the inputstream otherwise JVM closes the stream
+	ByteArrayOutputStream tempOutputStream = new ByteArrayOutputStream();
+	try {
+	    IOUtils.copy(blob.getPayload().getInput(), tempOutputStream);
+	} catch (IOException e1) {
+	    // TODO Auto-generated catch block
+	    e1.printStackTrace();
 	}
 
-	@Override
-	public boolean blobExists(String container, String blobName) {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Not yet implemented.");
+	createPathRecursively(container, orionBlob.getProperties()
+		.getParentPath().split(OrionConstantValues.PATH_DELIMITER));
+
+	api.createBlob(getUserLocation(), container, orionBlob.getProperties()
+		.getParentPath(), orionBlob);
+	// put contents in case it has some contents
+	if (orionBlob.getProperties().getType() == BlobType.FILE_BLOB) {
+	    orionBlob.setPayload(tempOutputStream.toByteArray());
+	    api.putBlob(getUserLocation(), container, orionBlob.getProperties()
+		    .getParentPath(), orionBlob);
 	}
 
-	@Override
-	public BlobMetadata blobMetadata(String container, String blobName) {
-		String parentPath = OrionUtils.fetchParentPath(blobName);
-		// Blob names must not start with a "/" since they are relative paths
-		// they will be automatically removed in case it starts with that
-		// Get the blob name
-		// Convert the blob name to it's metadata file name and fetch it
-		return blobProps2BlobMetadata.apply(api.getMetadata(getUserLocation(),
-				container, parentPath, OrionUtils
-						.getMetadataFileName(OrionUtils.fetchName(blobName,
-								parentPath))));
+	createMetadata(container, orionBlob);
+	return null;
+    }
+
+    @Override
+    public String putBlob(String arg0, Blob arg1, PutOptions arg2) {
+	// TODO Auto-generated method stub
+	throw new IllegalStateException("Not yet implemented.");
+    }
+
+    @Override
+    public void removeBlob(String arg0, String arg1) {
+	// TODO Auto-generated method stub
+	throw new IllegalStateException("Not yet implemented.");
+    }
+
+    private boolean createMetadata(String container, OrionBlob blob) {
+
+	return api.createMetadataFolder(getUserLocation(), container, blob
+		.getProperties().getParentPath())
+		&&
+		// Create metadata file
+		api.createMetadata(getUserLocation(), container, blob
+			.getProperties().getParentPath(), OrionUtils
+			.getMetadataFileName(blob.getProperties().getName())) &&
+		// Add metadata file contents
+		api.putMetadata(getUserLocation(), container, blob
+			.getProperties().getParentPath(), blob);
+
+    }
+
+    /**
+     * Create the non existing paths starting from index 0
+     * 
+     * @param container
+     * @param pathArray
+     */
+    private void createPathRecursively(String container, String[] pathArray) {
+	if (pathArray.length == 1) {
+	    return;
 	}
+	String parentPath = "";
+	int startIndex = 0;
 
-	@Override
-	public boolean containerExists(String container) {
-		return api.containerExists(getUserLocation(), container);
+	for (String path : pathArray) {
+	    if (!api.folderExists(getUserLocation(), container, path)) {
+		break;
+	    }
+	    startIndex++;
+	    parentPath = parentPath + path;
 	}
-
-	@Override
-	public void deleteContainer(String container) {
-		api.deleteContainer(getUserLocation(), container);
+	for (; startIndex < pathArray.length; startIndex++) {
+	    putBlob(container,
+		    blobUtils.blobBuilder().payload("")
+			    .name(parentPath + pathArray[startIndex])
+			    .type(StorageType.FOLDER).build());
+	    parentPath = parentPath + pathArray[startIndex]
+		    + OrionConstantValues.PATH_DELIMITER;
 	}
-
-	@Override
-	public boolean createContainerInLocation(Location location, String container) {
-		return this.api.createContainerAsAProject(getUserLocation(), container);
-	}
-
-	@Override
-	public boolean createContainerInLocation(Location arg0, String arg1,
-			CreateContainerOptions arg2) {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Not yet implemented.");
-	}
-
-	@Override
-	protected boolean deleteAndVerifyContainerGone(String container) {
-		return api.deleteContainer(getUserLocation(), container);
-	}
-
-	@Override
-	public Blob getBlob(String arg0, String arg1, GetOptions arg2) {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Not yet implemented.");
-	}
-
-	private String getUserLocation() {
-		return this.workspaceName;
-	}
-
-	@Override
-	public PageSet<? extends StorageMetadata> list() {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Not yet implemented.");
-	}
-
-	@Override
-	public PageSet<? extends StorageMetadata> list(String arg0,
-			ListContainerOptions arg1) {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Not yet implemented.");
-	}
-
-	@Override
-	public String putBlob(String container, Blob blob) {
-		OrionBlob orionBlob = this.blob2OrionBlob.apply(blob);
-		createPathRecursively(container, orionBlob.getProperties()
-				.getParentPath().split(OrionConstantValues.PATH_DELIMITER));
-
-		api.createBlob(getUserLocation(), container, orionBlob.getProperties()
-				.getParentPath(), orionBlob);
-		// put contents in case it has some contents
-		if (orionBlob.getProperties().getType() == BlobType.FILE_BLOB) {
-			api.putBlob(getUserLocation(), container, orionBlob.getProperties()
-					.getParentPath(), orionBlob);
-		}
-
-		createMetadata(container, orionBlob);
-		return null;
-	}
-
-	@Override
-	public String putBlob(String arg0, Blob arg1, PutOptions arg2) {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Not yet implemented.");
-	}
-
-	@Override
-	public void removeBlob(String arg0, String arg1) {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Not yet implemented.");
-	}
-
-	private boolean createMetadata(String container, OrionBlob blob) {
-
-		return api.createMetadataFolder(getUserLocation(), container, blob
-				.getProperties().getParentPath())
-				&&
-				// Create metadata file
-				api.createMetadata(getUserLocation(), container, blob
-						.getProperties().getParentPath(), OrionUtils
-						.getMetadataFileName(blob.getProperties().getName())) &&
-				// Add metadata file contents
-				api.putMetadata(getUserLocation(), container, blob
-						.getProperties().getParentPath(), blob);
-
-	}
-
-	/**
-	 * Create the non existing paths starting from index 0
-	 * 
-	 * @param container
-	 * @param pathArray
-	 */
-	private void createPathRecursively(String container, String[] pathArray) {
-		if (pathArray.length == 1) {
-			return;
-		}
-		String parentPath = "";
-		int startIndex = 0;
-
-		for (String path : pathArray) {
-			if (!api.folderExists(getUserLocation(), container, path)) {
-				break;
-			}
-			startIndex++;
-			parentPath = parentPath + path;
-		}
-		for (; startIndex < pathArray.length; startIndex++) {
-			putBlob(container,
-					blobUtils.blobBuilder().payload("")
-							.name(parentPath + pathArray[startIndex])
-							.type(StorageType.FOLDER).build());
-			parentPath = parentPath + pathArray[startIndex]
-					+ OrionConstantValues.PATH_DELIMITER;
-		}
-	}
+    }
 
 }
--- src/test/java/org/jclouds/orion/blobstore/integration/OrionBlobStoreTests.java
+++ src/test/java/org/jclouds/orion/blobstore/integration/OrionBlobStoreTests.java
@@ -1,7 +1,10 @@
 package org.jclouds.orion.blobstore.integration;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.Calendar;
 
+import org.apache.commons.io.FileUtils;
 import org.jclouds.ContextBuilder;
 import org.jclouds.blobstore.BlobStore;
 import org.jclouds.blobstore.BlobStoreContext;
@@ -14,91 +17,112 @@ import org.testng.annotations.Test;
 @Test(groups = "unit", testName = "OrionApiMetadataTest")
 public class OrionBlobStoreTests {
 
-	private BlobStore blobStore;
+    private BlobStore blobStore;
 
-	@Test
-	protected void containerExists() throws Exception {
-		String container = "Container+"
-				+ Calendar.getInstance().getTimeInMillis();
-		blobStore.createContainerInLocation(null, container);
-		Assert.assertTrue(blobStore.containerExists(container),
-				"Container SHOULD exist");
-		Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
-				.getInstance().getTimeInMillis())),
-				"Container SHOULD NOT exist");
-	}
+    @Test
+    protected void containerExists() throws Exception {
+	String container = "Container+"
+		+ Calendar.getInstance().getTimeInMillis();
+	blobStore.createContainerInLocation(null, container);
+	Assert.assertTrue(blobStore.containerExists(container),
+		"Container SHOULD exist");
+	Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
+		.getInstance().getTimeInMillis())),
+		"Container SHOULD NOT exist");
+    }
 
-	@Test
-	protected void createContainer() throws Exception {
-		blobStore.createContainerInLocation(null, "Container+"
-				+ Calendar.getInstance().getTimeInMillis());
-	}
+    @Test
+    protected void createContainer() throws Exception {
+	blobStore.createContainerInLocation(null, "Container+"
+		+ Calendar.getInstance().getTimeInMillis());
+    }
 
-	@Test
-	protected void deleteContainer() throws Exception {
-		String container = "Container+"
-				+ Calendar.getInstance().getTimeInMillis();
-		blobStore.deleteContainer(container);
-		blobStore.createContainerInLocation(null, container);
-		Assert.assertTrue(blobStore.containerExists(container),
-				"Container SHOULD exist");
-		Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
-				.getInstance().getTimeInMillis())),
-				"Container SHOULD NOT exist");
-	}
+    @Test
+    protected void deleteContainer() throws Exception {
+	String container = "Container+"
+		+ Calendar.getInstance().getTimeInMillis();
+	blobStore.deleteContainer(container);
+	blobStore.createContainerInLocation(null, container);
+	Assert.assertTrue(blobStore.containerExists(container),
+		"Container SHOULD exist");
+	Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
+		.getInstance().getTimeInMillis())),
+		"Container SHOULD NOT exist");
+    }
 
-	@Test
-	protected void clearContainer() throws Exception {
-		String container = "Container+"
-				+ Calendar.getInstance().getTimeInMillis();
-		blobStore.createContainerInLocation(null, container);
-		Assert.assertTrue(blobStore.containerExists(container),
-				"Container SHOULD exist");
-		Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
-				.getInstance().getTimeInMillis())),
-				"Container SHOULD NOT exist");
-	}
+    @Test
+    protected void clearContainer() throws Exception {
+	String container = "Container+"
+		+ Calendar.getInstance().getTimeInMillis();
+	blobStore.createContainerInLocation(null, container);
+	Assert.assertTrue(blobStore.containerExists(container),
+		"Container SHOULD exist");
+	Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
+		.getInstance().getTimeInMillis())),
+		"Container SHOULD NOT exist");
+    }
 
-	@Test
-	protected void putBlob() throws Exception {
-		String container = "Container+"
-				+ Calendar.getInstance().getTimeInMillis();
-		blobStore.createContainerInLocation(null, container);
-		Assert.assertTrue(blobStore.containerExists(container),
-				"Container SHOULD exist");
-		Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
-				.getInstance().getTimeInMillis())),
-				"Container SHOULD NOT exist");
-		String blobName = "/level1/level2/Blob+"
-				+ Calendar.getInstance().getTimeInMillis();
-		Blob blob = blobStore.blobBuilder(blobName).build();
-		blob.setPayload("PutBlobTest");
-		blobStore.putBlob(container, blob);
-	}
+    @Test
+    protected void putBlob() throws Exception {
+	String container = "Container+"
+		+ Calendar.getInstance().getTimeInMillis();
+	blobStore.createContainerInLocation(null, container);
+	Assert.assertTrue(blobStore.containerExists(container),
+		"Container SHOULD exist");
+	Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
+		.getInstance().getTimeInMillis())),
+		"Container SHOULD NOT exist");
+	String blobName = "/level1/level2/Blob+"
+		+ Calendar.getInstance().getTimeInMillis();
+	Blob blob = blobStore.blobBuilder(blobName).build();
+	blob.setPayload("PutBlobTest");
+	blobStore.putBlob(container, blob);
+    }
 
-	@Test
-	protected void getBlobMetadata() throws Exception {
-		String container = "Container+"
-				+ Calendar.getInstance().getTimeInMillis();
-		blobStore.createContainerInLocation(null, container);
-		String blobName = "/level1/level2/Blob+"
-				+ Calendar.getInstance().getTimeInMillis();
-		Blob blob = blobStore.blobBuilder(blobName).build();
-		blob.setPayload("PutBlobTest");
-		blob.getMetadata().getUserMetadata().put("test", "test");
-		blobStore.putBlob(container, blob);
-		BlobMetadata metadata = blobStore.blobMetadata(container, blobName);
-		Assert.assertEquals(metadata.getUserMetadata().containsKey("test"),
-				true, "user metadata is not there");
+    @Test
+    protected void putBigBlob() throws Exception {
+	String container = "Container+"
+		+ Calendar.getInstance().getTimeInMillis();
+	blobStore.createContainerInLocation(null, container);
+	Assert.assertTrue(blobStore.containerExists(container),
+		"Container SHOULD exist");
+	Assert.assertTrue(!blobStore.containerExists(String.valueOf(Calendar
+		.getInstance().getTimeInMillis())),
+		"Container SHOULD NOT exist");
+	String blobName = "/level1/level2/Blob+"
+		+ Calendar.getInstance().getTimeInMillis();
+	Blob blob = blobStore.blobBuilder(blobName).build();
+	String pathName = getClass().getClassLoader()
+		.getResource("Moodle.csar").getPath();
+	File testFile = new File(pathName);
+	InputStream iStream = FileUtils.openInputStream(testFile);
+	blob.setPayload(iStream);
+	blobStore.putBlob(container, blob);
+    }
 
-	}
+    @Test
+    protected void getBlobMetadata() throws Exception {
+	String container = "Container+"
+		+ Calendar.getInstance().getTimeInMillis();
+	blobStore.createContainerInLocation(null, container);
+	String blobName = "/level1/level2/Blob+"
+		+ Calendar.getInstance().getTimeInMillis();
+	Blob blob = blobStore.blobBuilder(blobName).build();
+	blob.setPayload("PutBlobTest");
+	blob.getMetadata().getUserMetadata().put("test", "test");
+	blobStore.putBlob(container, blob);
+	BlobMetadata metadata = blobStore.blobMetadata(container, blobName);
+	Assert.assertEquals(metadata.getUserMetadata().containsKey("test"),
+		true, "user metadata is not there");
 
-	@BeforeSuite
-	protected void setUp() throws Exception {
-		BlobStoreContext context = ContextBuilder.newBuilder("orionblob")
-				.endpoint("http://127.0.0.1:8080")
-				.credentials("timur", "123456").build(BlobStoreContext.class);
-		// create a container in the default location
-		blobStore = context.getBlobStore();
-	}
+    }
+
+    @BeforeSuite
+    protected void setUp() throws Exception {
+	BlobStoreContext context = ContextBuilder.newBuilder("orionblob")
+		.endpoint("http://127.0.0.1:8080")
+		.credentials("timur", "123456").build(BlobStoreContext.class);
+	// create a container in the default location
+	blobStore = context.getBlobStore();
+    }
 }
